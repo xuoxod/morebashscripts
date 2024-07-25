fb8d4352e2f25d8413b82196aa5b15bec6371d55
rick xuoxod <xuoxod@gmail.com>
2024-07-24 20:49:29 -0400

    Fixed dev env error

--- whatsmyip4address.sh
+++ whatsmyip4address.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 
 for I in $(ip -4 addr show | awk -F : '{print $2}' | grep -E "[a-z0-9][^\b(lo)\b\b(docker*)\b]{2}" | awk '{gsub(/[ \t\n\r]/,"");print}'); do
-    netface="$I";ip -4 addr show "${netface}" | grep -E "inet (([0-9])\.){4}*" | awk '{print $2}' | awk -F "/" '{print $1}'
+    netface="$I"
+    ip -4 addr show "${netface}" | grep -E "inet (([0-9])\.){4}*" | awk '{print $2}' | awk -F "/" '{print $1}'
 done
